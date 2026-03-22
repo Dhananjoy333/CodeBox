35f5dc3093a27d3bbfbffd04ad9fd2642d37d8bc
Dhananjoy <dhananjoybrahma333@gmail.com>
2026-03-22 17:33:36 +0530

    dashboard ui implemented

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { useUser } from '@clerk/nextjs'
 import axios from 'axios'
 import { UserDetailContext } from '@/context/UserDetailContext'
+import Header from './_components/Header'
 
 function Provider({
   children,
@@ -26,6 +27,9 @@ function Provider({
   return( 
   <NextThemesProvider {...props}>
     <UserDetailContext.Provider value={{userDetail,setUserDetail}}>
+      <div className='flex flex-col items-center'>
+        <Header/> 
+      </div>
       {children}
     </UserDetailContext.Provider>
   </NextThemesProvider>
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Hero from "./_components/Hero";
 export default function Home() {
   return (
     <div className="flex flex-col items-center">
-      <Header/>
       <Hero/>
     </div>
   );
