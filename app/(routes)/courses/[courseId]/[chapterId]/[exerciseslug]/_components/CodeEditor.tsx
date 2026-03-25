273ce8b76361240cdd5d88e54e0da2a911812ec8
Dhananjoy <dhananjoybrahma333@gmail.com>
2026-03-25 15:01:26 +0530

    removed react-splitter due to outdated version and added allotment

--- app/(routes)/courses/[courseId]/[chapterId]/[exerciseslug]/_components/CodeEditor.tsx
+++ app/(routes)/courses/[courseId]/[chapterId]/[exerciseslug]/_components/CodeEditor.tsx
@@ -1,12 +1,6 @@
-import {
-  SandpackProvider,
-  SandpackLayout,
-  SandpackCodeEditor,
-  SandpackPreview,
-  useSandpack,
-} from "@codesandbox/sandpack-react";
-import SplitterLayout from "react-splitter-layout";
-import 'react-splitter-layout/lib/index.css';
+import { SandpackProvider, SandpackLayout, SandpackCodeEditor, SandpackPreview, useSandpack } from "@codesandbox/sandpack-react";
+import { Allotment } from "allotment";
+import "allotment/dist/style.css"; 
 import { CourseExercise } from "../page";
 import { Button } from "@/components/ui/button";
 import { nightOwl } from "@codesandbox/sandpack-themes";
@@ -14,81 +8,87 @@ import { useParams } from "next/navigation";
 import axios from "axios";
 import { toast } from "sonner";
 
-type Props={
-    courseExerciseData:CourseExercise | undefined,
-    loading:boolean
-}
+type Props = {
+  courseExerciseData: CourseExercise | undefined;
+  loading: boolean;
+};
 
-const CodeEditorChildren = ({onCompleteExercise,IsCompleted}:any)=>{
-    const {sandpack} = useSandpack();
-    return(
-        <div className="font-game absolute bottom-40 flex gap-5 right-5">
-            <Button variant={'pixel'} size={'lg'} className="text-xl" onClick={()=>sandpack.runSandpack()}>
-                Run Code
-            </Button>
-            <Button variant={'pixel'} disabled={IsCompleted} className="bg-[#a3e534] text-xl" size={'lg'} onClick={()=>onCompleteExercise()}>
-               {IsCompleted ? "Already Completed !" : "Mark Completed !"} 
-            </Button>
-        </div>
-    )
-}
+const CodeEditorChildren = ({ onCompleteExercise, IsCompleted }: any) => {
+  const { sandpack } = useSandpack();
+  return (
+    <div className="font-game absolute bottom-0 flex gap-5 right-5">
+      <Button variant={'pixel'} size={'lg'} className="text-xl" onClick={() => sandpack.runSandpack()}>
+        Run Code
+      </Button>
+      <Button 
+        variant={'pixel'} 
+        disabled={IsCompleted} 
+        className="bg-[#a3e534] text-xl" 
+        size={'lg'} 
+        onClick={() => onCompleteExercise()}
+      >
+        {IsCompleted ? "Already Completed !" : "Mark Completed !"}
+      </Button>
+    </div>
+  );
+};
 
-function CodeEditor({courseExerciseData,loading}:Props) {
+function CodeEditor({ courseExerciseData, loading }: Props) {
+  const { exerciseslug } = useParams();
+  const exerciseIndex = courseExerciseData?.exercises?.findIndex(item => item.slug == exerciseslug);
+  const IsCompleted = courseExerciseData?.completedExercise?.find(item => item?.exerciseId == Number(exerciseIndex) + 1);
 
-    const {exerciseslug} = useParams()
-    const exerciseIndex = courseExerciseData?.exercises?.findIndex(item=>item.slug == exerciseslug)
-
-    const IsCompleted = courseExerciseData?.completedExercise?.find(item=>item?.exerciseId==Number(exerciseIndex)+1)
-
-    const onCompleteExercise = async()=>{
-
-        if(exerciseIndex==undefined)return
-
-        const result = await axios.post('/api/exercise/complete',{
-            courseId:courseExerciseData?.courseId,
-            chapterId:courseExerciseData?.chapterId,
-            exerciseId:exerciseIndex + 1,
-            xpEarned:courseExerciseData?.exercises[exerciseIndex].xp
-        })
-        toast.success("Exercise Completed!")
-    }
+  const onCompleteExercise = async () => {
+    if (exerciseIndex == undefined) return;
+    const result = await axios.post('/api/exercise/complete', {
+      courseId: courseExerciseData?.courseId,
+      chapterId: courseExerciseData?.chapterId,
+      exerciseId: exerciseIndex + 1,
+      xpEarned: courseExerciseData?.exercises[exerciseIndex].xp
+    });
+    toast.success("Exercise Completed!");
+  };
 
   return (
-    <div className="h-full"> 
-        <SandpackProvider
-        //@ts-ignore
-        template={courseExerciseData?.editorType??'react'}
+    <div className="h-full">
+      <SandpackProvider
+        template={(courseExerciseData?.editorType as any) ?? 'react'}
         theme={nightOwl}
-        style={{height:'100vh'}}
-        files = {courseExerciseData?.exerciseData?.exercisesContent?.starterCode}
+        style={{ height: '100%' }} 
+        files={courseExerciseData?.exerciseData?.exercisesContent?.starterCode}
         options={{
-            autorun:false,
-            autoReload:false
-        }}
-        >    
-            <SandpackLayout style={{height:'100%'}}>
-                <SplitterLayout
-                    percentage
-                    primaryMinSize={30}
-                    secondaryMinSize={30}
-                    secondaryInitialSize={50}>
-                    <div className="relative h-full ">
-                        <SandpackCodeEditor style={{height:'100%'}} 
-                        showTabs
-                        showRunButton={false}/>
-                        <CodeEditorChildren onCompleteExercise={onCompleteExercise}
-                        IsCompleted={IsCompleted}/>
-                    </div>
-                    <SandpackPreview style={{height:'100%'}} 
-                    showNavigator
-                    showOpenInCodeSandbox={false}
-                    showOpenNewtab
-                    />
-                </SplitterLayout>
-            </SandpackLayout>
-        </SandpackProvider>
+          autorun: false,
+          autoReload: false,
+        }}>
+        <SandpackLayout style={{ height: '100%', border: 'none' }}>
+          <Allotment>
+            <Allotment.Pane minSize={300}>
+
+              <div className="relative h-full w-full overflow-hidden"> 
+                <SandpackCodeEditor 
+                  style={{ height: '100%' }} 
+                  showTabs 
+                  showRunButton={false} 
+                />
+
+                <div className="absolute bottom-10 right-5 z-10">
+                  <CodeEditorChildren onCompleteExercise={onCompleteExercise} IsCompleted={IsCompleted} />
+                </div>
+              </div>
+            </Allotment.Pane>
+
+            <Allotment.Pane minSize={300}>
+              <SandpackPreview 
+                style={{ height: '100%' }} 
+                showNavigator 
+                showOpenInCodeSandbox={false} 
+              />
+            </Allotment.Pane>
+          </Allotment>
+        </SandpackLayout>
+      </SandpackProvider>
     </div>
-  )
+  );
 }
 
-export default CodeEditor
+export default CodeEditor;
